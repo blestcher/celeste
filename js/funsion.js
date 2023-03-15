52f32b87603a129ba43309909174841f3a0a5b00
Celeste Blestcher <blestcherceleste@gmail.com>
2023-03-15 14:49:36 -0300

    subir cv

---
+++ js/funsion.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').addEventListener('onclick'){
+    console.log("Gracias por consultar el CV ");
+    console.log("Espero que le haya gustado");
+    console.log("Saludos cordiales");
+}
